ea6b58c3fe96734c526d9f5c95cf21d655fb16c5
Karl <eber.karl@gmail.com>
2020-02-22 22:30:58 -0500

    End of hackathon progress

--- code/SightOfSoundApp/app/src/main/java/com/example/sightofsound/MainActivity.java
+++ code/SightOfSoundApp/app/src/main/java/com/example/sightofsound/MainActivity.java
@@ -2,31 +2,16 @@ package com.example.sightofsound;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Build;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import com.google.ar.core.Anchor;
 import com.google.ar.core.AugmentedFace;
 import com.google.ar.core.Frame;
-import com.google.ar.sceneform.AnchorNode;
-import com.google.ar.sceneform.math.Vector3;
-import com.google.ar.sceneform.rendering.Color;
-import com.google.ar.sceneform.rendering.MaterialFactory;
-import com.google.ar.sceneform.rendering.ModelRenderable;
 import com.google.ar.sceneform.rendering.Renderable;
-import com.google.ar.sceneform.rendering.ShapeFactory;
-import com.google.ar.sceneform.rendering.Texture;
 import com.google.ar.sceneform.rendering.ViewRenderable;
-import com.google.ar.sceneform.ux.ArFragment;
 import com.google.ar.sceneform.ux.AugmentedFaceNode;
-import com.google.ar.sceneform.ux.TransformableNode;
 
 import java.util.Collection;
-
-import static com.google.ar.sceneform.rendering.ShapeFactory.makeSphere;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -41,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
        CustomSpeechBubble customArFragment = (CustomSpeechBubble) getSupportFragmentManager().findFragmentById(R.id.arFragment);
        SpeechFrame speechFrame = new SpeechFrame();
        speechFrame.setTimer(0);
+       speechFrame.setOldTimer(0);
 
        customArFragment.getArSceneView().setCameraStreamRenderPriority(Renderable.RENDER_PRIORITY_FIRST);
        customArFragment.getArSceneView().getScene().addOnUpdateListener(frameTime -> {
@@ -59,9 +45,6 @@ public class MainActivity extends AppCompatActivity {
            TextView subtitleView = null;
 
            String character = "a";
-           String testSubtitle= character;
-
-           int oldTimer = speechFrame.getTimerInt();
 
            for (AugmentedFace augmentedFace : augmentedFaces) {
                if(!isAdded){
@@ -78,14 +61,16 @@ public class MainActivity extends AppCompatActivity {
                subtitleView = testViewRenderable.getView().findViewById(R.id.SubtitleWidget);
                speechFrame.addFrame(frameTime.getDeltaSeconds());
 
-               if(speechFrame.getTimerInt() != oldTimer){
+               if(speechFrame.getTimerInt() != speechFrame.getOldTimer()){
                    //do something once a second
-                   oldTimer = speechFrame.getTimerInt();
+                   speechFrame.setOldTimer(speechFrame.getTimerInt());
 
-                   testSubtitle += character;
+                   speechFrame.setCurrentSubtitle(speechFrame.getCurrentSubtitle() + character);
                }
-               subtitleView.setText(testSubtitle);
 
+               //subtitleView.setText(speechFrame.getCurrentSubtitle());
+               String s = speechFrame.getCurrentSubtitle();
+               subtitleView.setText(s);
                if (speechFrame.getTimer() >  100000){
 
                }
--- code/SightOfSoundApp/app/src/main/java/com/example/sightofsound/SpeechFrame.java
+++ code/SightOfSoundApp/app/src/main/java/com/example/sightofsound/SpeechFrame.java
@@ -1,17 +1,23 @@
 package com.example.sightofsound;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SpeechFrame {
     private float timer = 0;
+    private int oldTimer;
+
+    private String currentSubtitle = "";
 
     public float getTimer() {
         return timer;
     }
-
     public int getTimerInt() {return (int) timer;}
+    public void setTimer(float timer) {this.timer = timer;}
 
-    public void setTimer(float timer) {
-        this.timer = timer;
-    }
+    public int getOldTimer() {return oldTimer;}
+    public void setOldTimer(int time){oldTimer = time;}
+
     public void reset(){
         timer = 0;
     }
@@ -19,4 +25,7 @@ public class SpeechFrame {
     public void addFrame(float time){
         timer = timer + time;
     }
+
+    public String getCurrentSubtitle() { return currentSubtitle; }
+    public void setCurrentSubtitle(String currentSubtitle) {this.currentSubtitle = currentSubtitle;}
 }
